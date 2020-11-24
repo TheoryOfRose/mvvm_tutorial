8e2c5cf96f07724ea5256f246d47aeec5ff967ea
theorose49 <theorose49@snu.ac.kr>
2020-11-25 02:17:27 +0900

    초기 커밋.

---
+++ MVVMDemo/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿// ---------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="AIRS Medical Inc">
+// Copyright (c) AIRS Medical Inc. All rights reserved.
+// See LICENSE file in the project root for full license information.
+// SwiftMR can not be copied and/or distributed without the express permission of AIRS Medical Inc.
+// </copyright>
+// ---------------------------------------------------------------------
+using System.Windows;
+
+namespace MVVMDemo
+{
+  /// <summary>
+  /// fdfd.
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MainWindow"/> class.
+    /// fdasfasdf.
+    /// </summary>
+    public MainWindow()
+    {
+        this.InitializeComponent();
+    }
+  }
+}
---
+++ MVVMDemo/Views/StudentView.xaml.cs
@@ -0,0 +1,27 @@
+﻿// ---------------------------------------------------------------------
+// <copyright file="StudentView.xaml.cs" company="AIRS Medical Inc">
+// Copyright (c) AIRS Medical Inc. All rights reserved.
+// See LICENSE file in the project root for full license information.
+// SwiftMR can not be copied and/or distributed without the express permission of AIRS Medical Inc.
+// </copyright>
+// ---------------------------------------------------------------------
+
+using System.Windows.Controls;
+
+namespace MVVMDemo.Views
+{
+    /// <summary>
+    /// StudentView.xaml에 대한 상호 작용 논리.
+    /// </summary>
+    public partial class StudentView : UserControl
+    {
+      /// <summary>
+      /// Initializes a new instance of the <see cref="StudentView"/> class.
+      /// </summary>
+      public StudentView()
+      {
+              this.InitializeComponent();
+              this.DataContext = new MVVMDemo.ViewModel.StudentViewModel();
+      }
+     }
+}
