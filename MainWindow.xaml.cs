d8779d593668fe4ad7cd4797568d2b3b9ab80a82
theorose49 <theorose49@snu.ac.kr>
2020-11-26 13:55:12 +0900

    jfioajf

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,7 +12,7 @@ namespace MVVMDemo
     /// <summary>
     /// MainWindow.
     /// </summary>
-    public partial class MainWindow : Window
+public partial class MainWindow : Window
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="MainWindow"/> class.
